32c210cbde3f0b0cb11c9becd3d4b969f68e97b7
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-02-16 02:55:42 +0330

    Add RandomNumberCrypto static class and GenerateByteRandomNumberCrypto method to generate random numbers whose result is a byte

---
+++ RandomNumber/RandomNumberUtilities/RandomCrypto.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomNumber.RandomNumberUtilities
+{
+    public static class RandomNumberCrypto
+    {
+        public static byte[] GenerateByteRandomNumberCrypto(int length)
+        {
+            using var cryptoPovider = RandomNumberGenerator.Create();
+
+            var randomNumber = new byte[length];
+            cryptoPovider.GetBytes(randomNumber);
+
+            return randomNumber;
+        }
+    }
+}
