7dfd4d20d05bb1d0e6192c5711b3a194f3c0d392
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-02-16 03:01:10 +0330

    Add RandomNumberCrypto static class and GenerateStringRandomNumberCrypto method to generate random numbers whose result is a string

--- RandomNumber/RandomNumberUtilities/RandomCrypto.cs
+++ RandomNumber/RandomNumberUtilities/RandomCrypto.cs
@@ -4,6 +4,8 @@ namespace RandomNumber.RandomNumberUtilities
 {
     public static class RandomNumberCrypto
     {
+        private static readonly string randomGUID = Guid.NewGuid().ToString();
+
         public static byte[] GenerateByteRandomNumberCrypto(int length)
         {
             using var cryptoPovider = RandomNumberGenerator.Create();
@@ -12,6 +14,17 @@ namespace RandomNumber.RandomNumberUtilities
             cryptoPovider.GetBytes(randomNumber);
 
             return randomNumber;
+        }
+
+        public static string GenerateStringRandomNumberCrypto(int length)
+        {
+            using var cryptoPovider = RandomNumberGenerator.Create();
+
+            var randomNumber = new byte[length];
+
+            cryptoPovider.GetBytes(randomNumber);
+
+            return new string(randomNumber.Select(x => randomGUID[x % randomGUID.Length]).ToArray());
         }
     }
 }
