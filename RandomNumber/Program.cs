87e62b6401ddc611ea152d39e01b57abb027df1b
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-02-23 20:19:41 +0330

    Using random number generation methods

--- RandomNumber/Program.cs
+++ RandomNumber/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RandomNumber.RandomNumberUtilities;
+
+var byteRandomNumber = RandomNumberCrypto.GenerateByteRandomNumberCrypto(10);
+var stringRandomNumber = RandomNumberCrypto.GenerateStringRandomNumberCrypto(10);
+var guidRandomNumber = RandomNumberCrypto.GenerateGUIDRandomNumberCrypto();
+
+Console.WriteLine($"Generate a random 'String' number: {stringRandomNumber}");
+Console.WriteLine($"Generate a random 'GUID' number: {guidRandomNumber}");
