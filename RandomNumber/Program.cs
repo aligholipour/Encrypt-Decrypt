47784ef5a3e1f2f5aba1e55bb261c40ae2cd3a64
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-13 18:02:44 +0430

    Added how to use Encrypt and Decrypt in AES algorithm

--- RandomNumber/Program.cs
+++ RandomNumber/Program.cs
@@ -1,8 +1,25 @@
 ﻿using RandomNumber.RandomNumberUtilities;
+using RandomNumber.Symmetric;
 
-var byteRandomNumber = RandomNumberCrypto.GenerateByteRandomNumberCrypto(10);
-var stringRandomNumber = RandomNumberCrypto.GenerateStringRandomNumberCrypto(10);
-var guidRandomNumber = RandomNumberCrypto.GenerateGUIDRandomNumberCrypto();
+//var byteRandomNumber = RandomNumberCrypto.GenerateByteRandomNumberCrypto(10);
+//var stringRandomNumber = RandomNumberCrypto.GenerateStringRandomNumberCrypto(10);
+//var guidRandomNumber = RandomNumberCrypto.GenerateGUIDRandomNumberCrypto();
 
-Console.WriteLine($"Generate a random 'String' number: {stringRandomNumber}");
-Console.WriteLine($"Generate a random 'GUID' number: {guidRandomNumber}");
+//Console.WriteLine($"Generate a random 'String' number: {stringRandomNumber}");
+//Console.WriteLine($"Generate a random 'GUID' number: {guidRandomNumber}");
+
+string plainText = "This is a test string";
+
+byte[] key, iv;
+
+SymmetricAes.GenerateKeyAndIV(out key, out iv);
+
+byte[] encrypted = SymmetricAes.Encrypt(plainText, key, iv);
+
+string decrypred = SymmetricAes.Decrypt(encrypted, key, iv);
+
+Console.WriteLine($"plainText: {plainText}");
+
+Console.WriteLine($"encrypted: {Convert.ToBase64String(encrypted)}");
+
+Console.WriteLine($"decrypred: {decrypred}");
