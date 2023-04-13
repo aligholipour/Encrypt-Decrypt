54a79f6320f4ac5bd788ec5f4633e1df78917fd5
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-13 17:59:45 +0430

    Added SymmetricAes algorithm and GenerateKeyAndIV method

---
+++ RandomNumber/Symmetric/SymmetricAes.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomNumber.Symmetric
+{
+    public class SymmetricAes
+    {
+        public static void GenerateKeyAndIV(out byte[] key, out byte[] iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                key = aes.Key;
+                iv = aes.IV;
+            }
+        }
+    }
+}
