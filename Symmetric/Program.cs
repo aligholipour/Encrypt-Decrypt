02190587d132d9a7bbb55cb2aca9d5f18c6fa405
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-14 18:47:47 +0430

    How to use the AES algorithm was added to the Symmetric project.

--- Symmetric/Program.cs
+++ Symmetric/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Symmetric.SymmetricUtilities;
+
+string plainText = "This is a test string";
+
+byte[] key, iv;
+
+SymmetricAes.GenerateKeyAndIV(out key, out iv);
+
+byte[] encrypted = SymmetricAes.Encrypt(plainText, key, iv);
+
+string decrypred = SymmetricAes.Decrypt(encrypted, key, iv);
+
+Console.WriteLine($"plainText: {plainText}");
+
+Console.WriteLine($"encrypted: {Convert.ToBase64String(encrypted)}");
+
+Console.WriteLine($"decrypred: {decrypred}");
